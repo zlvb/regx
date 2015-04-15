b7215fda0a11a880a0771d679f4bb42044376eda
ZhangLi <37255537@qq.com>
2015-04-15 17:29:10 +0800

    ...

--- regxtest.py
+++ regxtest.py
@@ -7,11 +7,15 @@
 5+
 5*
 5?
+ord(last_c), ord(gettrans(regstr, idx+1])+1)
 '''
+
 EQUL = 1
 COUNT = 2
 ANY = 3
 TREE = 4
+DOT = 5
+RANGE = 6
 
 class Node:
     def __init__(self, ntype, parent = None):
@@ -20,17 +24,70 @@ class Node:
         self.children = []
         self.parent = parent
 
+def displaynode(node):
+    if node.type == EQUL:
+        return '[%d] %s\n' % (node.type, node.c)
+    elif node.type == COUNT:
+        return '[%d] %s\n' % (node.type, str(node.c))
+    elif node.type == RANGE:
+        return '[%d] %s-%s\n' % (node.type, node.children[0], node.children[1])
+    elif node.type == DOT:
+        return '[%d] %s\n' % (node.type, '.')
+
+    ast = []
+    for n in node.children:
+        ast.append(displaynode(n))
+
+    return ''.join(ast)
+
+
 class RegX:
     def __init__(self, regstr):
         self.curnode = Node(TREE)
         self.tokens = self.curnode.children
         self.parseregx(regstr)
 
+    def gettrans(self, regstr, idx):
+        return regstr[idx]
+
     def parseany(self, regstr, idx):
         regstr_len = len(regstr)
+        last_c = None
         while idx < regstr_len:
-            if regstr[idx] == '-' and regstr[idx+1] != ']' and regstr[idx-1] != '[':
-                for c in range(ord(regstr[idx-1]), ord(regstr[idx-1]))
+            if regstr[idx] == '\\':
+                last_c = gettrans(regstr,idx+1)
+                newnode = Node(EQUL, self.curnode)
+                newnode.c = last_c
+                self.tokens.append(newnode)
+                idx += 2
+            elif regstr[idx] == ']':
+                idx += 1
+                break
+            elif regstr[idx] == '-':
+                newnode = Node(RANGE, self.curnode)
+                newnode.children.append(last_c)
+                newnode.children.append(self.gettrans(regstr, idx+1))
+                self.tokens.append(newnode)
+                idx+=2
+            else:
+                last_c = regstr[idx]
+                newnode = Node(EQUL, self.curnode)
+                newnode.c = last_c
+                self.tokens.append(newnode)
+                idx += 1
+        return idx
+
+    def parsecount(self, regstr, idx):
+        regstr_len = len(regstr)
+        while idx < regstr_len:
+            if regstr[idx] == '}':
+                self.curnode.c = int(''.join(self.tokens))
+                idx += 1
+                break
+            else:
+                self.tokens.append(regstr[idx])
+                idx += 1
+        return idx
 
     def parseregx(self, regstr, idx = 0):
         regstr_len = len(regstr)
@@ -38,38 +95,50 @@ class RegX:
             if regstr[idx] == '[':
                 newnode = Node(ANY, self.curnode)
                 self.tokens.append(newnode)
+                self.curnode = newnode
+                self.tokens = newnode.children
+                idx+=1
                 idx = self.parseany(regstr, idx)
+                self.curnode = self.curnode.parent
+                self.tokens = self.curnode.children
             elif regstr[idx] == '{':
                 newnode = Node(COUNT, self.curnode)
                 self.tokens.insert(-1, newnode)
-                idx+=1
-            elif regstr[idx] == '(':
-                newnode = Node(TREE, self.curnode)
                 self.curnode = newnode
                 self.tokens = newnode.children
-                idx = self.parseregx(regstr, idx)
-            elif regstr[idx] == ')':
+                idx+=1
+                idx = self.parsecount(regstr, idx)
                 self.curnode = self.curnode.parent
                 self.tokens = self.curnode.children
+            elif regstr[idx] == '(':
+                newnode = Node(TREE, self.curnode)
+                self.tokens.append(newnode)
+                self.curnode = newnode
+                self.tokens = newnode.children
+                idx+=1
+                idx = self.parseregx(regstr, idx)
+                self.curnode = self.curnode.parent
+                self.tokens = self.curnode.children
+            elif regstr[idx] == ')':
                 idx+=1
                 break
-            elif regstr[idx] == '?':
-                newnode = Node(COUNT, self.curnode)
-                newnode.c = regstr[idx]
-                self.tokens.insert(-1, newnode)
-                idx+=1
-            elif regstr[idx] == '+':
-                newnode = Node(COUNT, self.curnode)
-                newnode.c = regstr[idx]
-                self.tokens.insert(-1, newnode)
-                idx+=1
-            elif regstr[idx] == '*':
+            elif regstr[idx] in '?*+':
                 newnode = Node(COUNT, self.curnode)
                 newnode.c = regstr[idx]
                 self.tokens.insert(-1, newnode)
                 idx+=1
             elif regstr[idx] == '.':
                 pass
-            else:
+            elif regstr[idx] == '\\':
                 pass
+            else:
+                newnode = Node(EQUL, self.curnode)
+                newnode.c = regstr[idx]
+                self.tokens.append(newnode)
+                idx+=1
         return idx
+
+import sys
+r = RegX('([123a-z]{4}){99}')
+for n in r.tokens:
+    sys.stdout.write(displaynode(n))
